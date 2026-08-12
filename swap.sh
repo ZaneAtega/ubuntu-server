05db70c12748fe90d27329c1cdbabeaf76f7df3b
zaneatega <zfaoatega@gmail.com>
2026-08-12 20:26:56 +0800

    🐧

---
+++ swap.sh
@@ -0,0 +1,6 @@
+fallocate -l 4G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness = 10' | sudo tee -a /etc/sysctl.conf
