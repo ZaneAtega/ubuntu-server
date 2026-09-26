19b61406b8bd95e93720ef3a18a7a9d74a01c32c
zaneatega <zfaoatega@gmail.com>
2026-09-26 11:43:43 +0800

    🐧

--- www/source/backup.sh
+++ www/source/backup.sh
@@ -1,4 +1,4 @@
-rm -f /www/backups/backups.zip
+rm -f /www/backup.log /www/backups/backups.zip /www/backups/db.zip /tmp/*/*.sql.gz
 
 zip='zip -r /www/backups/backups.zip /www/wwwroot/ -x "*/uploads/*" "*/wp-content/uploads/*" "*/wp-admin/*" "*/wp-includes/*" "*/h-cache/*"'
 
@@ -20,7 +20,7 @@ for site in /www/wwwroot/*; do
     user=$(grep -oP "define\s*\(\s*['\"]DB_USER['\"]\s*,\s*['\"]\K[^'\"]+" "$wp_config")
     password=$(grep -oP "define\s*\(\s*['\"]DB_PASSWORD['\"]\s*,\s*['\"]\K[^'\"]+" "$wp_config")
 
-    sql+=" && mysqldump -u $user -p'$password' $user | gzip > $TMP_DIR/$user.sql.gz"
+    sql+=" && echo $user && mysqldump -u $user -p'$password' $user | gzip > $TMP_DIR/$user.sql.gz"
 done
 
 eval "$zip$sql"
